b12268092b8b020927fcd600523eb55531ac9a3b
coolbox <pete@vinterior.co>
2019-08-17 23:14:07 +0100

    Redirect to playlists.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,7 @@
 
 class LoginController < ApplicationController
   def new
-    session[:user_return_to] = "http://localhost:3000/"
+    session[:user_return_to] = playlists_url
     redirect_to user_spotify_omniauth_authorize_url
   end
 end
